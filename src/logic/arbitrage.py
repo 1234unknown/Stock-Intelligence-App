ca658e2decc6c6d7d3907029beeb99f7aee3bd10
1234unknown <99817487+1234unknown@users.noreply.github.com>
2025-04-16 14:58:49 -0500

    Update arbitrage.py

--- src/logic/arbitrage.py
+++ src/logic/arbitrage.py
@@ -1,17 +1,12 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import zscore
 
-def analyze_arbitrage(data1: pd.DataFrame, data2: pd.DataFrame) -> dict:
-    merged = pd.merge(data1['Close'], data2['Close'], left_index=True, right_index=True, suffixes=("_1", "_2"))
-    merged.dropna(inplace=True)
-
-    spread = merged['Close_1'] - merged['Close_2']
-    corr = merged['Close_1'].corr(merged['Close_2'])
-    z_scores = zscore(spread)
-
+def analyze_arbitrage(data1, data2):
+    min_len = min(len(data1), len(data2))
+    spread = data1['Close'].iloc[-min_len:] - data2['Close'].iloc[-min_len:]
+    z_score = (spread - spread.mean()) / spread.std()
     return {
         'spread': spread,
-        'correlation': round(corr, 4),
-        'z_score': z_scores
+        'z_score': z_score,
+        'correlation': data1['Close'].iloc[-min_len:].corr(data2['Close'].iloc[-min_len:])
     }
